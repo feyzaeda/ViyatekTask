5834a1ce50d467a87ffc1abefd026f97d0c540b4
feyzaeda <fturk17@posta.pau.edu.tr>
2021-10-07 12:37:07 +0300

    edit fragments xml

--- app/src/main/java/com/example/viyatektask/main/MainActivity.kt
+++ app/src/main/java/com/example/viyatektask/main/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initUi()
